f1b23c8a995d5d0ea379ccdd046deaada5e1ada2
colin-nwk <collinsnwoko05@gmail.com>
2023-06-09 15:26:18 +0100

    database schema, migrations and models

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Travel;
-use App\Observers\TravelObserver;
+
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -23,18 +22,12 @@ class EventServiceProvider extends ServiceProvider
     ];
 
 
-    //! delete and use the method in the boot() method below
-    protected $observer = [
-        Travel::class => [TravelObserver::class]
-    ];
 
     /**
      * Register any events for your application.
      */
     public function boot(): void
     {
-        //! make it a class above, to register and observer
-        // Travel::observe(TravelObserver::class);
     }
 
 
@@ -50,18 +43,4 @@ class EventServiceProvider extends ServiceProvider
     /**
      * @return mixed
      */
-    public function getObserver()
-    {
-        return $this->observer;
-    }
-
-    /**
-     * @param mixed $observer
-     * @return self
-     */
-    public function setObserver($observer): self
-    {
-        $this->observer = $observer;
-        return $this;
-    }
 }
