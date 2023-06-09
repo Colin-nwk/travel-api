1245f5431abe042083b3a91c20522f6e45337a73
colin-nwk <collinsnwoko05@gmail.com>
2023-06-09 14:29:32 +0100

    observer added

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Travel;
+use App\Observers\TravelObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -20,13 +22,22 @@ class EventServiceProvider extends ServiceProvider
         ],
     ];
 
+
+    //! delete and use the method in the boot() method below
+    protected $observer = [
+        Travel::class => [TravelObserver::class]
+    ];
+
     /**
      * Register any events for your application.
      */
     public function boot(): void
     {
-        //
+        //! make it a class above, to register and observer
+        // Travel::observe(TravelObserver::class);
     }
+
+
 
     /**
      * Determine if events and listeners should be automatically discovered.
@@ -34,5 +45,23 @@ class EventServiceProvider extends ServiceProvider
     public function shouldDiscoverEvents(): bool
     {
         return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getObserver()
+    {
+        return $this->observer;
+    }
+
+    /**
+     * @param mixed $observer
+     * @return self
+     */
+    public function setObserver($observer): self
+    {
+        $this->observer = $observer;
+        return $this;
     }
 }
